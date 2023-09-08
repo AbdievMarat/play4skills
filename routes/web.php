5e3d1ecd7d6fa2107fbbac300dc7fffcd5c43e26
AbdievMarat <abdiev.m.t@gmail.com>
2023-09-08 17:43:25 +0600

    feat: edit rating url

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,7 @@ Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 Route::get('register', [LoginController::class, 'showRegisterForm'])->name('showRegisterForm');
 Route::post('register', [LoginController::class, 'register'])->name('register');
 
-Route::get('rating_display', [RatingController::class, 'display'])->name('rating_display');
+Route::get('rating_display_dima', [RatingController::class, 'display'])->name('rating_display');
 Route::get('rating/{userId}', [RatingController::class, 'getDetailWithUser'])->name('getDetailWithUser');
 
 Route::prefix('admin')->name('admin.')->middleware(['auth', 'role:admin'])->group(function () {
