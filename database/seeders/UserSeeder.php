344d8d1c824643cffb7db58f341423313f468b9f
AbdievMarat <abdiev.m.t@gmail.com>
2023-11-03 13:47:54 +0600

    fix: login seed

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -24,13 +24,13 @@ class UserSeeder extends Seeder
         $studentRole = Role::query()->where('name', '=', 'student')->first();
 
         $users = [
-            ["name" => "Команда 1", "email" => "command1@play.com", "decrypted_password" => "B9npEq", "password" => Hash::make('B9npEq')],
-            ["name" => "Команда 2", "email" => "command2@play.com", "decrypted_password" => "MB7ESg", "password" => Hash::make('MB7ESg')],
-            ["name" => "Команда 3", "email" => "command3@play.com", "decrypted_password" => "2oXKR9", "password" => Hash::make('2oXKR9')],
+            ["name" => "Команда 1", "email" => "command1@play4skills.com", "decrypted_password" => "B9npEq", "password" => Hash::make('B9npEq')],
+            ["name" => "Команда 2", "email" => "command2@play4skills.com", "decrypted_password" => "MB7ESg", "password" => Hash::make('MB7ESg')],
+            ["name" => "Команда 3", "email" => "command3@play4skills.com", "decrypted_password" => "2oXKR9", "password" => Hash::make('2oXKR9')],
             ["name" => "Команда 4", "email" => "command4@play.com", "decrypted_password" => "wMFux4", "password" => Hash::make('wMFux4')],
-            ["name" => "Команда 5", "email" => "command5@play.com", "decrypted_password" => "bPSsxl", "password" => Hash::make('bPSsxl')],
-            ["name" => "Команда 6", "email" => "command6@play.com", "decrypted_password" => "oDvDDP", "password" => Hash::make('oDvDDP')],
-            ["name" => "Команда 7", "email" => "command7@play.com", "decrypted_password" => "BOLczG", "password" => Hash::make('BOLczG')],
+            ["name" => "Команда 5", "email" => "command5@play4skills.com", "decrypted_password" => "bPSsxl", "password" => Hash::make('bPSsxl')],
+            ["name" => "Команда 6", "email" => "command6@play4skills.com", "decrypted_password" => "oDvDDP", "password" => Hash::make('oDvDDP')],
+            ["name" => "Команда 7", "email" => "command7@play4skills.com", "decrypted_password" => "BOLczG", "password" => Hash::make('BOLczG')],
         ];
 
         foreach ($users as $user) {
