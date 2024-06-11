30820ea123ec943a25f3c5ecf139c67e543c47cf
AbdievMarat <abdiev.m.t@gmail.com>
2024-06-11 09:25:32 +0600

    fix: column command text

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('command')->nullable();
+            $table->text('command')->nullable();
             $table->string('avatar')->nullable();
             $table->boolean('access_sent')->default(false);
             $table->timestamp('email_verified_at')->nullable();
